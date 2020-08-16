c9e8eae82ddc797fd8f35b782075fda4caae0b03
Javad Mnjd <javad.mnjd@hotmail.com>
2020-08-16 15:43:46 +0430

    add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/(*.)+(spec|test).[jt]s?(x)'],
+  testEnvironment: 'node',
+  restoreMocks: true,
+  clearMocks: true,
+  resetMocks: true,
+  verbose: true,
+};
