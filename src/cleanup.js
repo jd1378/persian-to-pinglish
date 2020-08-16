0f423b9a742cd25cf2e31fec65da2e8403727b10
Javad Mnjd <javad.mnjd@hotmail.com>
2020-08-16 16:10:08 +0430

    add other T form

--- src/cleanup.js
+++ src/cleanup.js
@@ -1,13 +1,15 @@
 const cleanupRegex = /[^0-9۰-۹a-zA-Zآابپتسجچحخدذرزژسشصضطظعغفقکگلمنوهیئ\s]/g;
 const alifRegex = /[اًٱأإ]/g; // not including آ due to different sound
 const yehRegex = /[يىۍې]/g; // not including ئ due to different sound
-const tehRegex = /[ەھهٔةہ]/g;
+const ehRegex = /[ەھهٔہ]/g;
 const kehRegex = /[ك]/g;
+const otherT = 'ة';
 
 function cleanup(str) {
   return str
     .replace(alifRegex, 'ا')
-    .replace(tehRegex, 'ه')
+    .replace(otherT, 'ت')
+    .replace(ehRegex, 'ه')
     .replace(kehRegex, 'ک')
     .replace(yehRegex, 'ی')
     .replace(cleanupRegex, '');
