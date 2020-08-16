c1d237486f4f41aea002bc08134d3ee57b5d5717
Javad Mnjd <javad.mnjd@hotmail.com>
2020-08-16 18:07:28 +0430

    first prototype

--- src/charmap.js
+++ src/charmap.js
@@ -1,8 +1,54 @@
-const confidentMap = {
-  ش: 'sh',
+import { zwnj } from './common';
+
+const confident = {
+  آ: 'aa',
+  ب: 'b',
+  پ: 'p',
+  پ: 'p',
+  ت: 't',
+  ط: 't',
+  ث: 's',
+  س: 's',
+  ص: 's',
+  ج: 'j',
   چ: 'ch',
-  ژ: 'zh',
+  ح: 'h',
   خ: 'kh',
+  د: 'd',
+  ذ: 'z',
+  ز: 'z',
+  ض: 'z',
+  ظ: 'z',
+  ر: 'r',
+  ش: 'sh',
+  ق: 'q',
+  غ: 'q',
+  ف: 'f',
+  ک: 'k',
+  گ: 'g',
+  ل: 'l',
+  م: 'm',
+  ن: 'n',
+  ژ: 'zh',
+};
+// letters that are not included in confidentMap:
+// ه و ی ا
+
+const uncertain = {
+  ا: 'a',
+  ی: 'y',
+  و: 'v',
+  ه: 'h',
 };
 
-export { confidentMap };
+const all = {
+  ...confident,
+  ...uncertain,
+};
+
+const compound = {
+  ['ه' + zwnj + 'ی']: 'eye',
+};
+
+export default { all, confident, compound, uncertain };
+export { all, confident, compound, uncertain };
--- src/convert.js
+++ src/convert.js
@@ -1,14 +1,82 @@
-function convert(word) {
-  if (!word) {
+import charmap from './charmap';
+
+/**
+ *
+ * @param {String} word
+ */
+function convert(_word) {
+  if (!_word) {
     return '';
   }
 
-  if (word === 'چیزی') {
-    return 'chizi';
-  }
-  if (word === 'بله') {
-    return 'bale';
-  }
+  // clone string
+  let word = `${_word}`;
+  let cWord = '';
+
+  do {
+    if (word.length === 1) {
+      if (word in charmap.uncertain) {
+        if (_word.length > 1) {
+          let eq = '';
+          switch (word) {
+            case 'ه':
+              eq = 'e';
+              break;
+            case 'ا':
+              eq = 'aa';
+              break;
+            case 'ی':
+              eq = 'i';
+              break;
+            case 'و':
+              eq = 'oo';
+              break;
+          }
+          cWord += eq;
+        } else {
+          cWord += charmap.uncertain[word];
+        }
+      } else {
+        cWord += charmap.confident[word] || '';
+      }
+      word = '';
+    } else if (word.length > 1) {
+      let fl = word.charAt(0);
+      let sl = word.charAt(1);
+
+      let eq = '';
+      if (fl in charmap.uncertain) {
+        cWord += charmap.uncertain[fl] || '';
+        cWord += charmap.all[sl] || '';
+      } else if (sl in charmap.uncertain) {
+        switch (sl) {
+          case 'و':
+            eq = 'oo';
+            break;
+          case 'ی':
+            eq = 'i';
+            break;
+          case 'ه':
+            eq = 'h';
+            break;
+          case 'ا':
+            eq = 'aa';
+            break;
+        }
+        cWord += charmap.all[fl] || '';
+        cWord += eq;
+      } else {
+        cWord += charmap.all[fl] || '';
+        // TODO: get most common connector vowel here
+        cWord += 'a';
+        cWord += charmap.all[sl] || '';
+      }
+
+      word = word.substr(2);
+    }
+  } while (word.length);
+
+  return cWord;
 }
 
 export default convert;
