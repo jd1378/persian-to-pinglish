f5ded931c4f9588f05d5f2e70d0fbd93869bfba4
Javad Mnjd <javad.mnjd@hotmail.com>
2020-08-16 16:34:07 +0430

    handle more cases

--- src/cleanup.js
+++ src/cleanup.js
@@ -1,17 +1,32 @@
-const cleanupRegex = /[^0-9۰-۹a-zA-Zآابپتسجچحخدذرزژسشصضطظعغفقکگلمنوهیئ\s]/g;
+// seperation for readability in editor
+const alifbaLetters = 'آابپتسجچحخدذرزژسشصضطظعغفقکگلمنوهیئ';
+const zwnj = '\u200c';
+
+const cleanupRegex = new RegExp(
+  `[^0-9۰-۹a-zA-Z${alifbaLetters}\s${zwnj}]`,
+  'g'
+);
 const alifRegex = /[اًٱأإ]/g; // not including آ due to different sound
 const yehRegex = /[يىۍې]/g; // not including ئ due to different sound
-const ehRegex = /[ەھهٔہ]/g;
+const ehRegex = /[ەھہ]/g;
 const kehRegex = /[ك]/g;
 const otherT = 'ة';
+const hamza = 'هٔ';
+
+// if theres a connected-yeh with a lone yeh in front of it
+const seperateYeAsWordRegex = new RegExp(`[ه]\\s[ی][^${alifbaLetters}]*`, 'g');
+
+const ownershipYeh = 'ه' + zwnj + 'ی';
 
 function cleanup(str) {
   return str
     .replace(alifRegex, 'ا')
+    .replace(hamza, ownershipYeh)
     .replace(otherT, 'ت')
     .replace(ehRegex, 'ه')
     .replace(kehRegex, 'ک')
     .replace(yehRegex, 'ی')
+    .replace(seperateYeAsWordRegex, ownershipYeh)
     .replace(cleanupRegex, '');
 }
 
