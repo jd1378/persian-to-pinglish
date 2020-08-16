420907fbd2f5c48682c241b2cbd3fa9e7c6e771e
Javad Mnjd <javad.mnjd@hotmail.com>
2020-08-16 19:33:16 +0430

    convert regex matches to string

--- src/getwords.js
+++ src/getwords.js
@@ -1,7 +1,9 @@
 const wordRegex = /[آابپتسجچحخدذرزژسشصضطظعغفقکگلمنوهیئ]+/g;
 
 function getWords(str) {
-  return Array.from(str.matchAll(wordRegex));
+  return Array.from(str.matchAll(wordRegex)).map((regMatch) =>
+    regMatch.toString()
+  );
 }
 
 export default getWords;
