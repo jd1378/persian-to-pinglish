72ae3313f22b7e6e0a060e3854c89aa4ca49d74d
Javad Mnjd <javad.mnjd@hotmail.com>
2020-08-19 03:22:03 +0430

    add heja pattern score calculation and tests

--- src/word-templates/index.js
+++ src/word-templates/index.js
@@ -1,4 +1,19 @@
 import arabic from './arabic';
 
-export default { arabic };
-export { arabic };
+function calculatePatternScore(pattern) {
+  let score = 0;
+  for (const heja of pattern) {
+    for (const vaj of heja) {
+      if (vaj.letter) {
+        score++;
+      }
+      if (vaj.type && vaj.type !== 'u') {
+        score++;
+      }
+    }
+  }
+  return score;
+}
+
+export default { calculatePatternScore, arabic };
+export { calculatePatternScore, arabic };
--- test/word-templates.test.js
+++ test/word-templates.test.js
@@ -1,22 +1,65 @@
-import arabic from '../src/word-templates/arabic';
+import { calculatePatternScore, arabic } from '../src/word-templates';
 import isEqual from 'lodash/isEqual';
+import { aM } from '../src/vaj';
 
 describe('word-templates', () => {
-  it("doesn't have exact duplicate patterns", () => {
-    let all = [...arabic];
-    let duplicates = [];
-    for (let wt of all) {
-      all.forEach((_) => {
-        if (isEqual(_.pattern, wt.pattern)) {
-          duplicates.push(_);
+  describe('patterns', () => {
+    describe('duplicates', () => {
+      it("doesn't have exact duplicate patterns", () => {
+        let all = [...arabic];
+        let duplicates = [];
+        for (let wt of all) {
+          all.forEach((_) => {
+            if (isEqual(_.pattern, wt.pattern)) {
+              duplicates.push(_);
+            }
+          });
+          if (duplicates.length > 1) {
+            break;
+          } else if (duplicates.length === 1) {
+            duplicates.pop();
+          }
         }
+        expect(duplicates.length).toBe(0);
       });
-      if (duplicates.length > 1) {
-        break;
-      } else if (duplicates.length === 1) {
-        duplicates.pop();
-      }
-    }
-    expect(duplicates.length).toBe(0);
+    });
+
+    describe('score calculation', () => {
+      it('counts non-empty letter as 1 score', () => {
+        const testPatt0 = [[{ letter: '' }]];
+        const testPatt1 = [[{ letter: '1' }]];
+
+        expect(calculatePatternScore(testPatt0)).toBe(0);
+        expect(calculatePatternScore(testPatt1)).toBe(1);
+      });
+
+      it('counts non-empty-non-standard letter as 1 score', () => {
+        const patt = [[{ letter: aM }]];
+
+        expect(calculatePatternScore(patt)).toBe(1);
+      });
+
+      it('counts letters in multiple vajs in the same heja', () => {
+        const patt = [[{ letter: '1' }, { letter: '1' }]];
+        expect(calculatePatternScore(patt)).toBe(2);
+      });
+
+      it('counts letters in multiple vajs across hejas', () => {
+        const patt = [[{ letter: '1' }], [{ letter: '1' }]];
+        expect(calculatePatternScore(patt)).toBe(2);
+      });
+
+      it('counts `u` type vaj as 0 score', () => {
+        const patt = [[{ type: 'u' }]];
+
+        expect(calculatePatternScore(patt)).toBe(0);
+      });
+
+      it('counts non empty type as 1 score', () => {
+        const patt = [[{ type: 'z' }]];
+
+        expect(calculatePatternScore(patt)).toBe(1);
+      });
+    });
   });
 });
