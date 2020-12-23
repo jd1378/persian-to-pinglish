bf4272477c24f4f5a373cc3ca3e47635c3f04af2
Javad Mnjd <javad.mnjd@hotmail.com>
2020-12-23 16:39:45 +0330

    update test a bit

--- .vscode/test.js
+++ .vscode/test.js
@@ -3,10 +3,11 @@ import {getPossibleDiacriticedWords, normalizeStr, toPinglishHejas, getBestWordM
 
 const word = 'بحث';
 
-const convertedWords = getPossibleDiacriticedWords(normalizeStr(word));
+/// const convertedWords = getPossibleDiacriticedWords(normalizeStr(word));
 
-console.log(convertedWords.map(wordParts => wordParts.map(h => h.map(v => v.letter)).flat().join('')+':'+toPinglishHejas(wordParts).flat().join('')).join('\n'));
+// console.log(convertedWords.map(wordParts => wordParts.map(h => h.map(v => v.letter)).flat().join('')+':'+toPinglishHejas(wordParts).flat().join('')).join('\n'));
 ///
 
-let bestMatch = getBestWordMatch(word).word;
-console.log(bestMatch.map(h => h.map(v => v.letter)).flat().join('')+':'+toPinglishHejas(bestMatch).flat().join(''));
+let bestMatch = getBestWordMatch(word);
+let bestMatchWord = bestMatch.word;
+console.log(bestMatchWord.map(h => h.map(v => v.letter)).flat().join('')+':'+toPinglishHejas(bestMatchWord).flat().join(''));
